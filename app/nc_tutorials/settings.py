06b5c63e7511eb8c5cd9fe2dc5a0d9c458d24e99
David Matendo <davidmatendo5@gmail.com>
2023-02-14 15:57:34 -0800

    Updated settings and urls.py

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-52-72-64-101.compute-1.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-52-72-64-101.compute-1.amazonaws.com',
+                 '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -126,10 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 if DEBUG:
-    STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
-    STATIC_ROOT=[os.path.join(BASE_DIR,"static")]
-
+    STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = '/static/'
 
